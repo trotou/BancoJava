2baa5f67b0178951618ad893b892b4e8bee79380
matheusribeiro <tico.ribeiro0396@gmail.com>
2020-08-17 17:44:53 -0300

    Mudanças significantes no código.
    Novos files com novas funcionalidades

--- BancoProject/src/Banco.java
+++ BancoProject/src/Banco.java
@@ -9,37 +9,3 @@ public class Banco {
 
 }
 
-
-class BancoConta
-{	
-	int balanco;
-	int transacaoPassada;
-	String Nome;
-	String id;
-	
-	void deposito(int quantidade)
-	{
-		if(quantidade != 0) {
-			balanco += quantidade;
-			transacaoPassada = quantidade;
-		}
-	}
-	void saca(int quantidade) {
-		if(quantidade != 0 ) {
-			balanco -= quantidade;
-			transacaoPassada = -quantidade;
-		}
-	}
-	void gettransacaoPassada() {
-		if(transacaoPassada > 0) {
-			System.out.println("Depositou " + transacaoPassada);
-		}
-		else if( transacaoPassada < 0) {
-			System.out.println("Sacou " + Math.abs(transacaoPassada));
-		}
-		else {
-			System.out.println("Não houve transferências");
-		}
-			
-	}
-}
---
+++ BancoProject/src/TestaCliente.java
@@ -0,0 +1,11 @@
+
+public class TestaCliente {
+
+	public static void main(String[] args) {
+		Conta cliente = new Conta("Matheus", "1234");
+		cliente.getNome();
+		cliente.showMenu();
+
+	}
+
+}
